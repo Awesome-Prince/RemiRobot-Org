d35ca18548c44c93cd39922c9871b53349ac7761
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-12-14 01:38:04 +0530

    RemiTheRobot

--- RemiRobot/__main__.py
+++ RemiRobot/__main__.py
@@ -227,7 +227,12 @@ def start(update: Update, context: CallbackContext):
 
         else:
             update.effective_message.reply_text(
-                PM_START_TEXT,
+                PM_START_TEXT.format(
+                   escape_markdown(context.bot.first_name),
+                   escape_markdown(first_name),
+                   escape_markdown(uptime),
+                   sql.num_users(),
+                   sql.num_chats()),
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
